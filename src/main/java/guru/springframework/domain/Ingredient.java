19c05f2355d7f0b8bf134b816cf2fac9e26d1bfc
Vlado <vladimir.jovanovic@logate.com>
2022-11-08 09:32:59 +0100

    Add a list of recipes on the index page

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -16,6 +16,15 @@ public class Ingredient {
     @OneToOne
     private UnitOfMeasure uom;
 
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
     public String getDescription() {
         return description;
     }
